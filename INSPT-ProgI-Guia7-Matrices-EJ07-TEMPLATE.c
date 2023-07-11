df219d7e9c557557494323fc8a995a96994bf729
Charly <carlosecimino@gmail.com>
2023-07-11 10:18:54 -0300

    Plantilla ej7 de matrices

---
+++ INSPT-ProgI-Guia7-Matrices-EJ07-TEMPLATE.c
@@ -0,0 +1,20 @@
+#include "utils.h" // Ajustar ruta relativa según corresponda
+const int CANT_ALUMNOS = 9;
+const int CANT_CLASES = 16;
+int main () {
+    cadena apellidos[CANT_ALUMNOS] = {"Alberti", "Azcuénaga", "Belgrano", "Castelli",
+                                      "Larrea", "Matheu", "Moreno", "Paso", "Saavedra"};
+    char asistencias[CANT_ALUMNOS][CANT_CLASES] = {
+      { 'P', 'P', 'P', 'P', 'P', 'P', 'T', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'T' },
+      { 'A', 'P', 'P', 'P', 'P', 'P', 'A', 'P', 'P', 'P', 'T', 'P', 'P', 'P', 'A', 'P' },
+      { 'T', 'P', 'A', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'A', 'P', 'P' },
+      { 'P', 'P', 'T', 'T', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A' },
+      { 'T', 'P', 'P', 'P', 'P', 'A', 'P', 'P', 'A', 'P', 'P', 'A', 'P', 'P', 'P', 'A' },
+      { 'P', 'P', 'P', 'P', 'T', 'T', 'P', 'P', 'P', 'P', 'A', 'P', 'P', 'P', 'P', 'A' },
+      { 'P', 'P', 'A', 'P', 'P', 'P', 'T', 'T', 'A', 'P', 'P', 'A', 'P', 'A', 'P', 'P' },
+      { 'P', 'P', 'P', 'P', 'A', 'A', 'T', 'P', 'P', 'P', 'P', 'A', 'A', 'P', 'P', 'A' },
+      { 'P', 'P', 'A', 'P', 'A', 'T', 'P', 'P', 'A', 'P', 'P', 'P', 'P', 'P', 'P', 'P' }
+    };
+    /* Desarrollar lo pedido... */
+    return 0;
+}
